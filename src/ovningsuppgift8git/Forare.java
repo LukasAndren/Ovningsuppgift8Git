3ce57ae5fee9bede08897ee51db4ce56770786cb
Lukas Andrén <jlukasaa@gmail.com>
2018-10-24 11:21:16 +0200

    Initial commit

---
+++ src/ovningsuppgift8git/Forare.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ovningsuppgift8git;
+
+/**
+ *
+ * @author buster
+ */
+public class Forare {
+    public String namn;
+    
+    public Forare(String namn){
+        this.namn = namn;
+    }
+    
+}
---
+++ src/ovningsuppgift8git/Ovningsuppgift8Git.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ovningsuppgift8git;
+
+/**
+ *
+ * @author buster
+ */
+public class Ovningsuppgift8Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Forare förare1 = new Forare("Erik");
+        Bilar bil1 = new Bilar("A6");
+        
+        System.out.println(bil1.modell);
+        System.out.println(förare1.namn);
+        
+    
+    }
+
+}
